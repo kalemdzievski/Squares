42781945d77010a49ad0c4a8bf8a74512295785f
Nenad Kaevik <nenadkaevik@gmail.com>
2014-08-31 15:00:41 +0200

    Napraven e GRIDOT so scriptice razbiras

---
+++ Assets/Grid.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public GameObject quad;
+	public int width = 6;
+	public int height = 6;
+
+	private GameObject [,] grid = new GameObject[6,6];
+
+	void Awake ()
+	{
+		for (int x=0; x<width; x++) {
+						for (int y=0; y<height; y++) {
+				GameObject gridQuad = (GameObject)Instantiate(quad);
+				gridQuad.transform.position = new Vector3(gridQuad.transform.position.x+4*x, gridQuad.transform.position.y+4*y, gridQuad.transform.position.z);
+				grid[x,y] = gridQuad;
+						}
+				}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
