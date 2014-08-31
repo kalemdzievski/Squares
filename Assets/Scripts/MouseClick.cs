af16d251a6c90712d747367085921a8db21d722f
Filip Nikolovski <fico_n@yahoo.com>
2014-08-31 18:41:02 +0200

    izmeni vo MouseClick

--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class MouseClick : MonoBehaviour {
 
+	private Vector3 vec = new Vector3(0,5,0);
+	private bool clicked = false;
 	void OnMouseDown() 
 	{
-		Debug.Log("Clicked");
+		Debug.Log ("clicked");
+		clicked = clicked ? false : true;
 	}
 	// Use this for initialization
 	void Start () {
@@ -14,6 +17,8 @@ public class MouseClick : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (clicked) {
+			renderer.transform.Rotate (vec);
+		}
 	}
 }
