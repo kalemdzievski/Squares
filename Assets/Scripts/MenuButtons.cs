36ac90848821e80e990501a0b405324115a28d5a
Nenad Kaevik <nenadkaevik@gmail.com>
2014-09-02 01:57:27 +0200

    Centrirano e glavnoto meni

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -2,28 +2,26 @@
 using System.Collections;
 
 public class MenuButtons : MonoBehaviour {
-
-	public int levo = 70;
+	
 	public int gore = 200;
 	public int shirina_ramka = 200;
 	public int visina_ramka = 240;
-	public int shirina_kopce = 180;
-	public int visina_kopce = 40;
+	public int visina_kopce = 20;
 		
 	void OnGUI () {
 			// Ova e ramkata okolu kopcinjata
-			GUI.Box(new Rect(levo,gore,shirina_ramka,visina_ramka), "Main Menu");
+		GUI.Box(new Rect(10,Screen.height/4,Screen.width-20,visina_ramka), "Main Menu");
 		//Tuka se kreirani kopcinjata
-		if(GUI.Button (new Rect (levo+10, gore+30, shirina_kopce, visina_kopce), "Start Game")) 
+		if(GUI.Button (new Rect (20, Screen.height/4 + 30, Screen.width-40 , visina_kopce), "Start Game")) 
 		{
 			Application.LoadLevel(1);
 		}
 
-		GUI.Button (new Rect (levo + 10, gore + visina_kopce + 40, shirina_kopce, visina_kopce), "Options");
+		GUI.Button (new Rect (20, Screen.height/4 + visina_kopce + 40, Screen.width-40, visina_kopce), "Options");
 
-		GUI.Button (new Rect (levo+10, gore+2*visina_kopce+50, shirina_kopce, visina_kopce), "High Scores");
+		GUI.Button (new Rect (20,Screen.height/4 + 2*visina_kopce+50, Screen.width-40, visina_kopce), "High Scores");
 
-		if(GUI.Button (new Rect (levo+10, gore+3*visina_kopce+60, shirina_kopce, visina_kopce), "Quit"))
+		if(GUI.Button (new Rect (20, Screen.height/4 + 3*visina_kopce+60, Screen.width-40, visina_kopce), "Quit"))
 		{
 			Application.Quit();
 		}
