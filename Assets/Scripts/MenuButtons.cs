1f67a564ae87c7ae983e277f2df6fd490e5e5a9f
Nenad Kaevik <nenadkaevik@gmail.com>
2014-09-02 15:28:41 +0200

    Dodaden e skin na menito

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -4,23 +4,26 @@ using System.Collections;
 public class MenuButtons : MonoBehaviour {
 		
 	public int gore = 200;
-	public int visina_ramka = 240;
-	public int visina_kopce = 20;
+	public int visina_kopce = 10;
+
+	public Texture2D btnImage = null;
+	public GUISkin skin = null;
 
 	void OnGUI () {
-			// Ova e ramkata okolu kopcinjata
-		GUI.Box(new Rect(10,Screen.height/4,Screen.width-20,visina_ramka), "Main Menu");
+
+		GUI.skin = skin;
+
 		//Tuka se kreirani kopcinjata
-		if(GUI.Button (new Rect (20, Screen.height/4 + 30, Screen.width-40 , visina_kopce), "Start Game")) 
+		if(GUI.Button (new Rect (0, Screen.height/4 + 30, Screen.width , visina_kopce), "Start Game")) 
 		{
 			Application.LoadLevel(1);
 		}
 
-		GUI.Button (new Rect (20, Screen.height/4 + visina_kopce + 40, Screen.width-40, visina_kopce), "Options");
+		GUI.Button (new Rect (0, Screen.height/4 + visina_kopce + 40, Screen.width, visina_kopce), "Options");
 
-		GUI.Button (new Rect (20,Screen.height/4 + 2*visina_kopce+50, Screen.width-40, visina_kopce), "High Scores");
+		GUI.Button (new Rect (0,Screen.height/4 + 2*visina_kopce+50, Screen.width, visina_kopce), "High Scores");
 
-		if(GUI.Button (new Rect (20, Screen.height/4 + 3*visina_kopce+60, Screen.width-40, visina_kopce), "Quit"))
+		if(GUI.Button (new Rect (0, Screen.height/4 + 3*visina_kopce+60, Screen.width, visina_kopce), "Quit"))
 		{
 			Application.Quit();
 		}
