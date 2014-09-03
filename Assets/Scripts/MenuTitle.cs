218a336f7c91738438d71540804dc27c9644669b
Nenad Kaevik <nenadkaevik@gmail.com>
2014-09-03 03:16:14 +0200

    Popravki na glavnoto meni so dodaden naslov

--- Assets/Scripts/MenuTitle.cs
+++ Assets/Scripts/MenuTitle.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class MenuTitle : MonoBehaviour {
+	
 
 	// Use this for initialization
 	void Start () {
-		transform.localScale = new Vector3 (1,1,1);
+
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		guiText.text = "SQUARES";
+		guiText.fontSize = Screen.width / 6;
+
 	}
 }
