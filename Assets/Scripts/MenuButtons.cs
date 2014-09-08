2b1b6827a9769e025a717544d6d558adaa4cf9f4
Nenad Kaevik <nenadkaevik@gmail.com>
2014-09-08 21:01:59 +0200

    Dodadena muzika vo igrata i zvuk pri stiskanje na kopcinjata vo menito

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -2,14 +2,13 @@
 using System.Collections;
 
 public class MenuButtons : MonoBehaviour {
-
-	public Texture2D btnImage = null;
+	
 	public GUISkin skin = null;
 	public string btnStart = string.Empty;
 	public string btnOptions = string.Empty;
 	public string btnScores = string.Empty;
 	public string btnExit = string.Empty;
-
+	public GameObject clicksound = null;
 
 
 	void OnGUI () {
@@ -20,16 +19,27 @@ public class MenuButtons : MonoBehaviour {
 		//Tuka se kreirani kopcinjata
 		if(GUI.Button (new Rect (0, Screen.height/3, Screen.width , Screen.height/11),btnStart)) 
 		{
+			clicksound.audio.Play();
+			DontDestroyOnLoad(clicksound);
 			Application.LoadLevel(1);
+
 		}
 
-		GUI.Button (new Rect (0, Screen.height / 3 + Screen.height / 11 + Screen.height / 20, Screen.width, Screen.height / 11), btnOptions);
+		if (GUI.Button (new Rect (0, Screen.height / 3 + Screen.height / 11 + Screen.height / 20, Screen.width, Screen.height / 11), btnOptions)) {
+
+			clicksound.audio.Play();
+			DontDestroyOnLoad(clicksound);
+		}
 
 
-		GUI.Button (new Rect (0,Screen.height/3 + 2*Screen.height/11 + 2*Screen.height/20, Screen.width, Screen.height/11), btnScores);
+		if (GUI.Button (new Rect (0, Screen.height / 3 + 2 * Screen.height / 11 + 2 * Screen.height / 20, Screen.width, Screen.height / 11), btnScores)) {
+			clicksound.audio.Play();
+			DontDestroyOnLoad(clicksound);
+		}
 
 		if(GUI.Button (new Rect (0, Screen.height/3 + 3*Screen.height/11 + 3*Screen.height/20, Screen.width, Screen.height/11), btnExit))
 		{
+			clicksound.audio.Play();
 			Application.Quit();
 		}
 		if (Input.GetKeyDown(KeyCode.Escape))
