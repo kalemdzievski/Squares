4d5ee50033c2e39bd2c11aa7e120ebd1d23b19b3
Filip Nikolovski <fico_n@yahoo.com>
2014-09-15 19:38:24 +0200

    izmena vo progress bar skriptata

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -7,15 +7,18 @@ public class ProgressBar : MonoBehaviour {
 	public float currWidth;
 	public float barHeight;
 	public float seconds;
-	public Texture progressBar;
+	public float fadeDuration;
 	public Color startColor;
 	public Color endColor;
+	public GUITexture progressBar;
 
 	// Use this for initialization
 	void Start () {
+		progressBar = GameObject.Find ("ProgressBar").guiTexture;
 		barWidth = Screen.width - 20.0f;
 		barHeight = 10.0f;
 		seconds = 10.0f;
+		fadeDuration = 2.0f;
 		startColor = guiTexture.color;
 		endColor = Color.red;
 		transform.position = Vector3.zero;
@@ -26,14 +29,29 @@ public class ProgressBar : MonoBehaviour {
 	void Update () {
 		if (currWidth <= 0) {
 			currWidth = barWidth;
-			guiTexture.color = startColor;
+			progressBar.color = startColor;
 		}
 		currWidth -= barWidth / seconds * Time.deltaTime;
-		guiTexture.color = Color.Lerp (guiTexture.color, endColor, Time.deltaTime / seconds);
+		progressBar.color = Color.Lerp (guiTexture.color, endColor, Time.deltaTime / seconds);
 	}
 
 	void OnGUI () {
-
-		guiTexture.pixelInset = new Rect (10 ,Screen.height - 18, currWidth, barHeight);
+		progressBar.pixelInset = new Rect (10 ,Screen.height - 18, currWidth, barHeight);
 	}
+
+	/*
+ 	private IEnumerator Fade (float startLevel, float endLevel, float time)
+	{
+		float speed = 1.0f/time;
+		
+		for (float t = 0.0f; t < 1.0; t += Time.deltaTime*speed)
+		{
+			float a = Mathf.Lerp(startLevel, endLevel, t);
+			popupText.font.material.color = new Color(popupText.font.material.color.r,
+			                                        popupText.font.material.color.g,
+			                                        popupText.font.material.color.b, a);
+			yield return 0;
+		}
+	}
+	*/
 }
