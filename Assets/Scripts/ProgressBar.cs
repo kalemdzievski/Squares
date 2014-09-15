b32f2ebc6cb4dabb35c372c694752c38daad8671
Filip Nikolovski <fico_n@yahoo.com>
2014-09-15 15:59:08 +0200

    progress bar izmeni

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -4,24 +4,28 @@ using System.Collections;
 public class ProgressBar : MonoBehaviour {
 	
 	public float barWidth;
+	public float currWidth;
 	public float barHeight;
-	public float timer;
+	public float seconds;
 	public Texture progressBar;
 
 	// Use this for initialization
 	void Start () {
 		barWidth = Screen.width - 20.0f;
 		barHeight = 12.0f;
-		timer = barWidth / 60.0f * Time.deltaTime;
-		Debug.Log ("BAR WIDTH: " + barWidth);
+		seconds = 60.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		barWidth -= timer;
+		if (currWidth <= 0) {
+			currWidth = barWidth;
+		}
+	
+		currWidth -= barWidth / seconds * Time.deltaTime;
 	}
 
 	void OnGUI () {
-		GUI.DrawTexture (new Rect (10.0f, 10.0f, barWidth, barHeight), progressBar, ScaleMode.StretchToFill, true, 1.0f);
+		GUI.DrawTexture (new Rect (10.0f, 10.0f, currWidth, barHeight), progressBar, ScaleMode.StretchToFill, true, 1.0f);
 	}
 }
