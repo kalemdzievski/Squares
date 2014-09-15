5bc8a87b09e8bde0ee263de6d3da620c41a18bab
Filip Nikolovski <fico_n@yahoo.com>
2014-09-15 17:05:52 +0200

    izmeni kaj progress barot

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -8,24 +8,32 @@ public class ProgressBar : MonoBehaviour {
 	public float barHeight;
 	public float seconds;
 	public Texture progressBar;
+	public Color startColor;
+	public Color endColor;
 
 	// Use this for initialization
 	void Start () {
 		barWidth = Screen.width - 20.0f;
 		barHeight = 12.0f;
-		seconds = 60.0f;
+		seconds = 10.0f;
+		startColor = guiTexture.color;
+		endColor = Color.red;
+		transform.position = Vector3.zero;
+		transform.localScale = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (currWidth <= 0) {
 			currWidth = barWidth;
+			guiTexture.color = startColor;
 		}
-	
 		currWidth -= barWidth / seconds * Time.deltaTime;
+		guiTexture.color = Color.Lerp (guiTexture.color, endColor, Time.deltaTime / seconds);
 	}
 
 	void OnGUI () {
-		GUI.DrawTexture (new Rect (10.0f, 10.0f, currWidth, barHeight), progressBar, ScaleMode.StretchToFill, true, 1.0f);
+
+		guiTexture.pixelInset = new Rect (10 ,Screen.height - 20, currWidth, barHeight);
 	}
 }
