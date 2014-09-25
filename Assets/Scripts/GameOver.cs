083d7d09e32fcc06bef70eab3a98278ab44cdaca
Nenad Kaevik <nenadkaevik@gmail.com>
2014-09-25 03:12:21 +0200

    Napraen e scorot da se pokazuva na game over

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -6,8 +6,11 @@ public class GameOver : MonoBehaviour {
 	public GUISkin skin;
 	public GUIStyle BackgroundFade;
 	public GUIStyle Title;
+	public GUIStyle ScoreStyle;
+	public GUIStyle ScoreValStyle;
 	public Font pauseMenuFont;
 	public Color pauseBoxColor;
+	public GUIText ScoreText;
 
 	private Texture2D MakeTex( int width, int height, Color col )
 	{
@@ -25,6 +28,7 @@ public class GameOver : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		pauseBoxColor = new Color (1.0f, 1.0f, 1.0f, 0.0f);
+		Time.timeScale = 0;
 	}
 	
 	// Update is called once per frame
@@ -35,18 +39,23 @@ public class GameOver : MonoBehaviour {
 	void OnGUI () {
 		GUI.skin = skin;
 		GUI.Box(new Rect(0, 0, Screen.width ,Screen.height), "", BackgroundFade);
-		GUI.Label (new Rect (0, Screen.height / 4.0f, Screen.width, 30), "GAME OVER", Title);
+		GUI.Label (new Rect (0, Screen.height / 3, Screen.width, 30), "GAME OVER", Title);
+		GUI.Label (new Rect (0, Screen.height / 10, Screen.width, 30), "SCORE", ScoreStyle);
+		GUI.Label (new Rect (0, Screen.height / 6, Screen.width, 30), GameObject.FindGameObjectWithTag ("Score").guiText.text.ToString(), ScoreValStyle);
 		BackgroundFade.normal.background = MakeTex( 2, 2, pauseBoxColor );
+		Title.fontSize = (int)Screen.width / 12;
+		ScoreStyle.fontSize = (int)Screen.width / 10;
+		ScoreValStyle.fontSize = (int)Screen.width / 10;
 
 		//Retry button
-		if(GUI.Button(new Rect(0, Screen.height/4 + Screen.height/10, Screen.width , Screen.height/11), "Retry")){
+		if(GUI.Button(new Rect(0, Screen.height/3 + Screen.height/10, Screen.width , Screen.height/11), "Retry")){
 			Application.LoadLevel(1);
 			Time.timeScale = 1;
 			AudioListener.volume = 1;
 		}
 		
 		//Main menu button
-		if(GUI.Button(new Rect(0, Screen.height/4 + 2*Screen.height/10, Screen.width , Screen.height/11), "Main Menu")){
+		if(GUI.Button(new Rect(0, Screen.height/3 + 2*Screen.height/10, Screen.width , Screen.height/11), "Main Menu")){
 			Application.LoadLevel(0);
 		}
 	}
