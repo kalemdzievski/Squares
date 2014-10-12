994c7c926c7ba04263eee9ad89cde42684d73284
Nenad Kaevik <nenadkaevik@gmail.com>
2014-10-12 23:29:33 +0200

    Funkcionalni brojaci za gadgeti

--- Assets/Resources/Scripts/Gadgets.cs
+++ Assets/Resources/Scripts/Gadgets.cs
@@ -20,6 +20,11 @@ public class Gadgets : MonoBehaviour {
 	public GUIContent FreezeTimeContent;
 	public GUIContent NoSquareContent;
 	public GUIContent FreeMoveContent;
+	public int FreezeCount;
+	public int MoveCount;
+	public int NoSquareCount;
+	public GUIStyle GadgetIndicators;
+	public Vector2 IndicatorOffset;
 
 	public bool freeMove;
 	public bool freezeTime;
@@ -38,10 +43,15 @@ public class Gadgets : MonoBehaviour {
 		gadgetsWidth  = (float)Screen.width / 3.2f;
 		gadgetsHeight = (float)Screen.width / 6;
 		offset = Screen.width / 3 - Screen.width / 3.2f;
+		GadgetIndicators.fontSize = Screen.width / 20;
 		//offset = gadgetsWidth + leftPosition;
 		FreezeTimeContent.image = FreezeTimeIcon;
 		NoSquareContent.image   = NoSquareIcon;
 		FreeMoveContent.image 	= FreeMoveIcon;
+		IndicatorOffset = GadgetIndicators.contentOffset;
+		IndicatorOffset.x -= Screen.width / 60;
+		IndicatorOffset.y += Screen.width / 70;
+		GadgetIndicators.contentOffset = IndicatorOffset;
 
 		freeMove   = false;
 		freezeTime = false;
@@ -61,17 +71,24 @@ public class Gadgets : MonoBehaviour {
 
 	void OnGUI () {
 
+
 		//Freeze time gadget
 		if (GUI.Button (new Rect (leftPosition, topPosition, gadgetsWidth, gadgetsHeight),FreezeTimeContent, FreezeTimeTex)) {
 			if(!freezeTime) {
+				if(FreezeCount!=0)
+				{
+				FreezeCount--;
 				freezeTime = true;
 				GameObject popup = (GameObject)Instantiate(popupText);
 				popup.GetComponent<GadgetsPopupText>().setText("Freeze time");
+				}
 			}
 		}
 
 		//Free move gadget
 		if (GUI.Button (new Rect (leftPosition + Screen.width / 3, topPosition, gadgetsWidth, gadgetsHeight),FreeMoveContent, FreeMoveTex)) {
+			if(MoveCount!=0){
+			MoveCount--;
 			freeMove = !freeMove;
 			GameObject popup = (GameObject)Instantiate(popupText);
 			popup.GetComponent<GadgetsPopupText>().setText("Free move");
@@ -93,13 +110,22 @@ public class Gadgets : MonoBehaviour {
 					}
 				}
 			}
+			}
 		}
 
 		//No squares gadget
 		if (GUI.Button (new Rect (leftPosition + 2*Screen.width / 3, topPosition, gadgetsWidth, gadgetsHeight),NoSquareContent, NoSquareTex)) {
-			noSquares = !noSquares;
-			GameObject popup = (GameObject)Instantiate(popupText);
-			popup.GetComponent<GadgetsPopupText>().setText("No squares next turn");
+			if(NoSquareCount!=0){
+				NoSquareCount--;
+				noSquares = !noSquares;
+				GameObject popup = (GameObject)Instantiate(popupText);
+				popup.GetComponent<GadgetsPopupText>().setText("No squares next turn");
+			}
+
 		}
+
+		GUI.Label (new Rect (leftPosition, topPosition, gadgetsWidth, gadgetsHeight), FreezeCount.ToString(),GadgetIndicators);
+		GUI.Label (new Rect (leftPosition + Screen.width / 3, topPosition, gadgetsWidth, gadgetsHeight), MoveCount.ToString(),GadgetIndicators);
+		GUI.Label (new Rect (leftPosition + 2*Screen.width / 3, topPosition, gadgetsWidth, gadgetsHeight), NoSquareCount.ToString(),GadgetIndicators);
 	}
 }
